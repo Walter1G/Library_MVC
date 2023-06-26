5eb4086730fe31333a8a60efd38a7aec0849ba7b
Walter Ndungu <2760walter@gmail.com>
2023-06-26 22:59:20 +0300

    Create Category List Page

--- LibraryWeb/Data/ApplicationDbContext.cs
+++ LibraryWeb/Data/ApplicationDbContext.cs
@@ -17,5 +17,15 @@ namespace LibraryWeb.Data
         //dbset to create a table 
         public DbSet<Category> Categories{ get; set; }
 
+        //overide onModelcreating to seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.Entity<Category>().HasData(
+               new Category { Id=1, Name="Action",DisplayOrder=1},
+               new Category { Id=2, Name="Scifi",DisplayOrder=2},
+               new Category { Id=3, Name="History",DisplayOrder=3}
+               );
+        }
+
     }
 }
