1abeb147de9e735b7a879cf3e6925ebc212b169f
Walter Ndungu <2760walter@gmail.com>
2023-06-26 11:34:28 +0300

    Added Category Table in the database plus the view

---
+++ LibraryWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LibraryWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        //establish connection between db and ef
+       
+       
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            //pass the connection string in the appsetting to the ctor by injecting it as a param as dbcontext option
+
+            
+        }
+        //dbset to create a table 
+        public DbSet<Category> Categories{ get; set; }
+
+    }
+}
