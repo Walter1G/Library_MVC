5e8f89dfb232dfb27fff41ba38f0209a302ef635
Walter Ndungu <2760walter@gmail.com>
2023-07-02 19:52:29 +0300

    Added Product and crud operation

---
+++ Library.DataAcess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Library.DataAcess.Repository.IRepository;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository:IRepository<Product>
+    {
+        void Update(Product obj);
+       
+    }
+}
---
+++ LibraryWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,108 @@
+﻿using Library.DataAccess.Data;
+using Library.DataAccess.Repository.IRepository;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]    
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+           
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created successfully";
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productfromdb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productfromdb == null) { return NotFound(); }
+
+
+            return View(productfromdb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productfromdb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productfromdb == null) { return NotFound(); }
+
+
+            return View(productfromdb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null) { return NotFound(); }
+
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product Deleted successfully";
+            return RedirectToAction("Index");
+
+
+
+
+        }
+    }
+}
