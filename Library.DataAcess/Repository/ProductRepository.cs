bbd8642ce8e1dcc14a432e7987b9a2f7ebbc818f
Walter1G <2760walter@gmail.com>
2023-07-16 20:52:28 +0300

    Display Image on update, handle image on upddate by custom code

--- Library.DataAcess/Repository/ProductRepository.cs
+++ Library.DataAcess/Repository/ProductRepository.cs
@@ -22,7 +22,31 @@ namespace Library.DataAccess.Repository
 
         public void Update(Product obj)
         {
-            _db.Products.Update(obj);
+            //_db.Products.Update(obj);
+
+            var objFromDb = _db.Products.FirstOrDefault(u=>u.Id == obj.Id);
+
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Description = obj.Description;
+                objFromDb.Price = obj.Price;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                //objFromDb.Category = obj.Category;
+                objFromDb.Author = obj.Author;
+                objFromDb.CategoryId = obj.CategoryId;
+
+                if(obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+
+
+            }
+
         }
     }
 }
