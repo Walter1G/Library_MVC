0d41877e0808d99c87eb1afd490a94e43bdb521b
Walter1G <2760walter@gmail.com>
2023-08-06 11:14:06 +0300

    Create Roles In Db

--- Library.Utility/SD.cs
+++ Library.Utility/SD.cs
@@ -8,5 +8,13 @@ namespace Library.Utility
 {
     public static class SD
     {
+        //Static details 
+
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
+
     }
 }
--- LibraryWeb/Program.cs
+++ LibraryWeb/Program.cs
@@ -3,6 +3,9 @@ using Library.DataAccess.Repository;
 using Library.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Library.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +14,9 @@ builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
